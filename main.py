b109ad8ed148c0cedd34480f4024ad6fb643397a
Mike <super.mvk@yandex.ru>
2019-11-09 02:59:34 +0300

    Make main.py to script-like entrypoint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import tkinter as tk
 import tkinter.ttk
 import matplotlib.pyplot as plt
@@ -20,27 +21,28 @@ exact, euler, improved_euler, runge_kutta
 solution, pointwise_error, total_approximation_error
 """
 
-ex = Exact(0, math.sqrt(0.5), 3)
-eu = Euler(0, math.sqrt(0.5), 3)
-ieu = ImprovedEuler(0, math.sqrt(0.5), 3)
-rk = RungeKutta(0, math.sqrt(0.5), 3)
-
-n = 10
-x = ex.x_list(n)
-y1 = ex.solve(n)
-y2 = eu.solve(n)
-y3 = ieu.solve(n)
-y4 = rk.solve(n)
-
-plt.plot(x, y1, label="exact")
-plt.plot(x, y2, label="euler")
-plt.plot(x, y3, label="improved euler")
-plt.plot(x, y4, label="runge kutta")
-plt.legend()
-plt.show()
-
 """
 root = tk.Tk()
 app = tk.Frame()
 app.mainloop()
 """
+
+if __name__ == "__main__":
+    ex = Exact(0, math.sqrt(0.5), 3)
+    eu = Euler(0, math.sqrt(0.5), 3)
+    ieu = ImprovedEuler(0, math.sqrt(0.5), 3)
+    rk = RungeKutta(0, math.sqrt(0.5), 3)
+
+    n = 10
+    x = ex.x_list(n)
+    y1 = ex.solve(n)
+    y2 = eu.solve(n)
+    y3 = ieu.solve(n)
+    y4 = rk.solve(n)
+
+    plt.plot(x, y1, label="exact")
+    plt.plot(x, y2, label="euler")
+    plt.plot(x, y3, label="improved euler")
+    plt.plot(x, y4, label="runge kutta")
+    plt.legend()
+    plt.show()
